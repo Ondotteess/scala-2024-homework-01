050f9b21b5d5ee8472aa2ed6eac3ea37fc6ecbba
Ondotteess <108961850+Ondotteess@users.noreply.github.com>
2024-09-26 22:06:57 +0700

    complete task

--- calculator.scala
+++ calculator.scala
@@ -1,24 +1,116 @@
-/** Software implementation of PROC (PROstoy Calculator) mk. 1 (or mk. 2).
-  *
-  * You should finish this procedure according to
-  * the reference described in `README.md` to complete
-  * the assignment.
-  */
+import scala.util.boundary, boundary.break
+
+class Memory {
+  var acc: Int = 0
+  var A: Int = 0
+  var B: Int = 0
+  var blink: Boolean = false
+}
+
+abstract class Command {
+  def execute(memory: Memory): Command
+}
+
+class Plus extends Command {
+  override def execute(memory: Memory): Command = {
+    memory.acc = memory.A + memory.B
+    memory.blink = false
+    this
+  }
+}
+
+class Minus extends Command {
+  override def execute(memory: Memory): Command = {
+    memory.acc = memory.A - memory.B
+    memory.blink = false
+    this
+  }
+}
+
+class Mult extends Command {
+  override def execute(memory: Memory): Command = {
+    memory.acc = memory.A * memory.B
+    memory.blink = false
+    this
+  }
+}
+
+class Division extends Command {
+  override def execute(memory: Memory): Command = {
+    if (memory.B == 0) {
+      memory.A = 0
+      memory.B = 0
+      memory.acc = 0
+    } else {
+      memory.acc = memory.A / memory.B
+    }
+    memory.blink = false
+    this
+  }
+}
+
+class Blink extends Command {
+  override def execute(memory: Memory): Command = {
+    memory.blink = !memory.blink
+    this
+  }
+}
+
+class Swap extends Command {
+  override def execute(memory: Memory): Command = {
+    val temp = memory.A
+    memory.A = memory.B
+    memory.B = temp
+    this
+  }
+}
+
+class Acc extends Command {
+  override def execute(memory: Memory): Command = {
+    if (memory.blink) {
+      memory.B = memory.acc
+    } else {
+      memory.A = memory.acc
+    }
+    memory.blink = !memory.blink
+    this
+  }
+}
+
+
+class LoadInt(val value: Int) extends Command {
+  override def execute(memory: Memory): Command = {
+    if (memory.blink) {
+      memory.B = value
+    } else {
+      memory.A = value
+    }
+    memory.blink = !memory.blink
+    this
+  }
+}
+
 @main def calculator(commands: String*): Unit = {
-  /** Converts given string `s` to integer.
-    *
-    * Throws [[NumberFormatException]] if `s` can't be converted to integer,
-    * but you shouldn't worry about it at this moment.
-    */
   def parseInt(s: String): Int = s.toInt
 
-  /** Representation of `acc` register. */
-  var acc: Int = 0
-  // define additional registers here
-
-  for (c <- commands) {
-    // implement your calculator's logic here
+  val mem = new Memory()
+  
+  boundary {
+    for (c <- commands) {
+      c match
+        case "+" => new Plus().execute(mem)
+        case "-" => new Minus().execute(mem)
+        case "*" => new Mult().execute(mem)
+        case "/" => new Division().execute(mem)
+        case "swap" => new Swap().execute(mem)
+        case "blink" => new Blink().execute(mem)
+        case "acc" => new Acc().execute(mem)
+        case "break" => break(None)
+        case s: String if s.matches("-?\\d+") =>
+          new LoadInt(s.toInt).execute(mem)
+        case _ => println("Unknown command")
+    }
   }
 
-  println(acc)
+  println(mem.acc)
 }
